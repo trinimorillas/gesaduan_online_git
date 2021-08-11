047a3800fad40e592588510bf42d1c93f4dc73bb
gcliment <guillerm.climent-pavia-external@capgemini.com>
2021-08-11 11:53:34 +0200

    Añadida escructura v2 a Detail, y acabado el DAO
    FALTRA PROBAR Y REVISAR SI FALLA

---
+++ modules/gesaduan-war/src/main/java/es/mercadona/gesaduan/dto/declaracionesdevalorapi/getvddetail/v2/restful/DataDTO.java
@@ -0,0 +1,80 @@
+package es.mercadona.gesaduan.dto.declaracionesdevalorapi.getvddetail.v2.restful;
+
+import java.util.List;
+
+import es.mercadona.gesaduan.dto.declaracionesdevalor.AbstractDTO;
+
+public class DataDTO extends AbstractDTO {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private VDCommonDataDTO commonData;
+	private VDHeaderDTO header;
+	private List<VDHistoricalDTO> historical;
+	private List<VDLineDTO> lineList;
+	private List<VDItemDTO> itemList;
+	
+	/**
+	 * @return the commonData
+	 */
+	public VDCommonDataDTO getCommonData() {
+		return commonData;
+	}
+	/**
+	 * @param commonData the commonData to set
+	 */
+	public void setCommonData(VDCommonDataDTO commonData) {
+		this.commonData = commonData;
+	}
+	/**
+	 * @return the header
+	 */
+	public VDHeaderDTO getHeader() {
+		return header;
+	}
+	/**
+	 * @param header the header to set
+	 */
+	public void setHeader(VDHeaderDTO header) {
+		this.header = header;
+	}
+	/**
+	 * @return the historical
+	 */
+	public List<VDHistoricalDTO> getHistorical() {
+		return historical;
+	}
+	/**
+	 * @param historical the historical to set
+	 */
+	public void setHistorical(List<VDHistoricalDTO> historical) {
+		this.historical = historical;
+	}
+	/**
+	 * @return the lineList
+	 */
+	public List<VDLineDTO> getLineList() {
+		return lineList;
+	}
+	/**
+	 * @param lineList the lineList to set
+	 */
+	public void setLineList(List<VDLineDTO> lineList) {
+		this.lineList = lineList;
+	}
+	/**
+	 * @return the itemList
+	 */
+	public List<VDItemDTO> getItemList() {
+		return itemList;
+	}
+	/**
+	 * @param itemList the itemList to set
+	 */
+	public void setItemList(List<VDItemDTO> itemList) {
+		this.itemList = itemList;
+	}
+
+	
+
+}
---
+++ modules/gesaduan-war/src/main/java/es/mercadona/gesaduan/dto/declaracionesdevalorapi/getvddetail/v2/restful/VDCommonDataDTO.java
@@ -0,0 +1,117 @@
+package es.mercadona.gesaduan.dto.declaracionesdevalorapi.getvddetail.v2.restful;
+
+import es.mercadona.gesaduan.dto.declaracionesdevalor.AbstractDTO;
+
+public class VDCommonDataDTO extends AbstractDTO {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private boolean isLastCurrent;
+	private boolean isValueDeclarationOk;
+	private boolean isNotified;
+	private String valueDeclarationDownloadDate;
+	private String exportDownloadDate;
+	private String importDownloadDate;
+	private boolean isAutomaticLoading;
+
+	/**
+	 * @return the isLastCurrent
+	 */
+	public boolean isIsLastCurrent() {
+		return isLastCurrent;
+	}
+
+	/**
+	 * @param isLastCurrent the isLastCurrent to set
+	 */
+	public void setLastCurrent(boolean isLastCurrent) {
+		this.isLastCurrent = isLastCurrent;
+	}
+
+	/**
+	 * @return the isValueDeclarationOk
+	 */
+	public boolean isIsValueDeclarationOk() {
+		return isValueDeclarationOk;
+	}
+
+	/**
+	 * @param isValueDeclarationOk the isValueDeclarationOk to set
+	 */
+	public void setValueDeclarationOk(boolean isValueDeclarationOk) {
+		this.isValueDeclarationOk = isValueDeclarationOk;
+	}
+
+	/**
+	 * @return the isNotified
+	 */
+	public boolean isIsNotified() {
+		return isNotified;
+	}
+
+	/**
+	 * @param isNotified the isNotified to set
+	 */
+	public void setNotified(boolean isNotified) {
+		this.isNotified = isNotified;
+	}
+
+	/**
+	 * @return the valueDeclarationDownloadDate
+	 */
+	public String getValueDeclarationDownloadDate() {
+		return valueDeclarationDownloadDate;
+	}
+
+	/**
+	 * @param valueDeclarationDownloadDate the valueDeclarationDownloadDate to set
+	 */
+	public void setValueDeclarationDownloadDate(String valueDeclarationDownloadDate) {
+		this.valueDeclarationDownloadDate = valueDeclarationDownloadDate;
+	}
+
+	/**
+	 * @return the exportDownloadDate
+	 */
+	public String getExportDownloadDate() {
+		return exportDownloadDate;
+	}
+
+	/**
+	 * @param exportDownloadDate the exportDownloadDate to set
+	 */
+	public void setExportDownloadDate(String exportDownloadDate) {
+		this.exportDownloadDate = exportDownloadDate;
+	}
+
+	/**
+	 * @return the importDownloadDate
+	 */
+	public String getImportDownloadDate() {
+		return importDownloadDate;
+	}
+
+	/**
+	 * @param importDownloadDate the importDownloadDate to set
+	 */
+	public void setImportDownloadDate(String importDownloadDate) {
+		this.importDownloadDate = importDownloadDate;
+	}
+
+	/**
+	 * @return the isAutomaticLoading
+	 */
+	public boolean isIsAutomaticLoading() {
+		return isAutomaticLoading;
+	}
+
+	/**
+	 * @param isAutomaticLoading the isAutomaticLoading to set
+	 */
+	public void setAutomaticLoading(boolean isAutomaticLoading) {
+		this.isAutomaticLoading = isAutomaticLoading;
+	}
+
+
+
+}
