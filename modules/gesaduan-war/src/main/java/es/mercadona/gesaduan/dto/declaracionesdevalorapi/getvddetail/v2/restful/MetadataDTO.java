1155d55607ce20109455d2f7a3a1777ef248d16a
CORP\amarti34 <alberto.martinez-cardona@capgemini.com>
2021-09-15 17:46:15 +0200

    Get Detalle devolucion v2, anyadir el nuevo parametro NUM_LINEA

---
+++ modules/gesaduan-war/src/main/java/es/mercadona/gesaduan/dto/declaracionesdevalorapi/getvddetail/v2/restful/MetadataDTO.java
@@ -0,0 +1,9 @@
+package es.mercadona.gesaduan.dto.declaracionesdevalorapi.getvddetail.v2.restful;
+
+import es.mercadona.gesaduan.dto.declaracionesdevalor.AbstractDTO;
+
+public class MetadataDTO extends AbstractDTO {
+
+	private static final long serialVersionUID = 1L;
+
+}
